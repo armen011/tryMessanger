843fa6aef8b8c5f359b7038f29fd45672789fc2a
ARMEN MKRTCHYAN <armen21mkrtchyan0616@gmail.com>
2021-08-19 19:14:39 +0400

    feat(build): build is pushed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { LiveChatLoaderProvider, Messenger } from "react-live-chat-loader";
+import Hello from "./hello";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello
+      <LiveChatLoaderProvider
+        provider="messenger"
+        providerKey="112237750634324"
+        appID="111222333444555"
+        locale="en_US"
+      >
+        <Messenger />
+      </LiveChatLoaderProvider>
     </div>
   );
 }
